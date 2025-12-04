3323cf26371ceb91b889e025bedf8f65652f02ce
akElements <akelements.dev@gmail.com>
2025-12-04 09:14:43 +0100

    Updated codegen to run treesitter build (c libs) before building c#

--- external/build_tree_sitter.bat
+++ external/build_tree_sitter.bat
@@ -1,4 +1,9 @@
-mkdir bin
+cd /d "%~dp0"
+
+if not exist bin (
+    mkdir bin
+)
+
 call zig cc -shared -target x86_64-windows-msvc -shared -O3 -DNDEBUG -o bin/tree-sitter.dll -Itree-sitter/lib/src -Itree-sitter/lib/include tree-sitter/lib/src/lib.c tree-sitter.def
 
 call zig cc -shared -target x86_64-windows-msvc -shared -O3 -DNDEBUG -o bin/tree-sitter-cpp.dll -Itree-sitter-cpp/src  -Itree-sitter/lib/include  tree-sitter-cpp/src/parser.c  tree-sitter-cpp/src/scanner.c tree-sitter-cpp.def
